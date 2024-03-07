a8e7b27611b61f96ec57fbfc727184db29baa3e8
LongPeng <longpeng96@qq.com>
2024-03-07 23:44:09 +0800

    feat: update

--- foo_test.go
+++ foo_test.go
@@ -2,10 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/bradfitz/gomemcache/memcache"
+	"github.com/go-redis/redis"
 	"testing"
 )
 
 func BenchmarkRedis(b *testing.B) {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < keyCount; j++ {
 			_, err := redisClient.Get(fmt.Sprintf("key%d", j)).Result()
@@ -17,6 +24,8 @@ func BenchmarkRedis(b *testing.B) {
 }
 
 func BenchmarkMemcached(b *testing.B) {
+	mcClient = memcache.New("localhost:11211")
+
 	for i := 0; i < b.N; i++ {
 		for j := 0; j < keyCount; j++ {
 			_, err := mcClient.Get(fmt.Sprintf("key%d", j))
